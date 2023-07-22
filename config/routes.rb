274271487b7e231ff92310da7fc071fd8ded229b
JuanFrancisco <juanarmijoreyes@gmail.com>
2023-07-22 02:19:49 -0400

    Completed project page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'portfolio/home'
+  get 'portfolio/projects'
+  get 'portfolio/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "articles#index
+  root "portfolio#home"
 end
